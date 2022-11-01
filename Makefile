27461c72680c59870b8bdf0976d17d1b16e3165e
MaartenDMT <maartendormenatteysen@hotmail.com>
2022-11-01 12:49:37 +0100

    adduser to server with channels

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 build:
-	go build -o /bin/channels
+	go build -o bin/channels
 
 run: build
 	go run ./bin/channels
 
 test:
-	go test -v ./...
+	go test -v  ./... --race
--- main.go
+++ main.go
@@ -2,14 +2,41 @@ package main
 
 import "fmt"
 
+type Server struct {
+	users  map[string]string
+	userch chan string
+}
+
+func NewServer() *Server {
+	return &Server{
+		users:  make(map[string]string),
+		userch: make(chan string),
+	}
+}
+
+func (s *Server) Start() {
+	go s.loop()
+}
+
+func (s *Server) loop() {
+	for {
+		user := <-s.userch
+		s.users[user] = user
+	}
+}
+
+func (s *Server) addUser(user string) {
+	s.users[user] = user
+}
+
 func main() {
-	userch := make(chan string)
-	
-	go func() {
-		userch <- "Bob" //blocking
-	}()
+}
 
-	user := <-userch
+func sendMessage(msgh chan<- string) {
+	msgh <- "hello!"
+}
 
-	fmt.Println(user)
+func readMessage(msghr <-chan string) {
+	msg := <-msghr
+	fmt.Println(msg)
 }
